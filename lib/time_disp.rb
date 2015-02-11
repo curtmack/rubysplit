acea8bd94be93059c3bcbad9c14c017af67c0924
Curtis Mackie <curtmackevo@gmail.com>
2015-02-10 21:58:19 -0600

    Added time_disp_diff for split rendering purposes

--- lib/time_disp.rb
+++ lib/time_disp.rb
@@ -19,4 +19,31 @@ class Numeric
             return "%2d:%02d.%02d" % [minutes, seconds, hundredths]
         end
     end
+
+    def time_disp_diff(other)
+        rem = (self - other).abs
+
+        hours = (rem / 3600).floor
+        rem -= (hours * 3600)
+
+        minutes = (rem / 60).floor
+        rem -= (minutes * 60)
+
+        seconds = rem.floor
+        rem -= seconds
+
+        hundredths = (rem * 100).round
+
+        sign = (self < other) ? "-" : "+" 
+
+        if (hours > 0)
+            return "%s%2d:%02d:%02d" % [sign, hours, minutes, seconds]
+        else
+            if (minutes > 0)
+                return "%s%2d:%02d.%02d" % [sign, minutes, seconds, hundredths]
+            else
+                return "%s%2d.%02d" % [sign, seconds, hundredths]
+            end
+        end
+    end
 end
--- spec/time_disp_spec.rb
+++ spec/time_disp_spec.rb
@@ -20,3 +20,24 @@ describe "Numeric#time_disp" do
         (3660.1).time_disp.should == " 1:01:00"
     end
 end
+
+describe "Numeric#time_disp_diff" do
+    it "exists" do
+        20.time_disp_diff(20)
+    end
+
+    it "shows the time in seconds to hundredths precision" do
+        4.time_disp_diff(2).should == "+ 2.00"
+        20.time_disp_diff(60.1).should == "-40.10"
+        2.time_disp_diff(2).should == "+ 0.00"
+    end
+
+    it "shows the time in minutes and seconds to thundredths precision" do
+        (90.1).time_disp_diff(10).should == "+ 1:20.10"
+        (10).time_disp_diff(690.1).should == "-11:20.10"
+    end 
+
+    it "shows the time in hours, minutes, seconds" do
+        (3670.1).time_disp_diff(10).should == "+ 1:01:00"
+    end
+end
